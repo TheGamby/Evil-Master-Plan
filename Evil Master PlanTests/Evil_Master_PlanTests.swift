6283dd2e269cd2cda4614df9af31b96158679898
TheGamby <ludhessen@yahoo.de>
2026-04-07 11:55:10 +0200

    Initial Commit

---
+++ Evil Master PlanTests/Evil_Master_PlanTests.swift
@@ -0,0 +1,16 @@
+//
+//  Evil_Master_PlanTests.swift
+//  Evil Master PlanTests
+//
+//  Created by Jürgen Reichardt-Kron on 07.04.26.
+//
+
+import Testing
+
+struct Evil_Master_PlanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Evil Master PlanUITests/Evil_Master_PlanUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Evil_Master_PlanUITestsLaunchTests.swift
+//  Evil Master PlanUITests
+//
+//  Created by Jürgen Reichardt-Kron on 07.04.26.
+//
+
+import XCTest
+
+final class Evil_Master_PlanUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
